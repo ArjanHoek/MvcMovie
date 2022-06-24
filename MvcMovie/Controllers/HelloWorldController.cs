10b3e7fdefa958b03ea005a4cf1197a35b9298c8
Arjan Hoek <arjanhoek5191@gmail.com>
2022-06-24 11:39:20 +0200

    add a controller

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // Every public method in a controller is callable as an HTTP endpoint
+       
+        // /HelloWorld/ > /Index/ is not required, since it is the default when no endpoint for this controller was given
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+
+        }
+    }
+}
