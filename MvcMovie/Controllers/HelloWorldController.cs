d8ad8852f73fba755887903b09decbbcb021f687
Arjan Hoek <arjanhoek5191@gmail.com>
2022-06-24 12:30:40 +0200

    add a view

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -8,17 +8,19 @@ namespace MvcMovie.Controllers
         // Every public method in a controller is callable as an HTTP endpoint
        
         // /HelloWorld/ > /Index/ is not required, since it is the default when no endpoint for this controller was given
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // /HelloWorld/Welcome/ 
         // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
 
+            return View();
         }
     }
 }
